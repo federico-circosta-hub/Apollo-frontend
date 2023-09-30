b6784775e33eb5bd67a407b14ddc608181ae1197
fridericus-hub <fed.circosta@gmail.com>
2023-09-30 14:52:01 +0200

    get patient api request improved: error control

--- src/physician/View/OtherComponents/PatientLine.jsx
+++ src/physician/View/OtherComponents/PatientLine.jsx
@@ -1,25 +1,23 @@
-import { format, } from 'date-fns';
+import { format } from "date-fns";
 
 export default function PatientLine(props) {
+  const select = () => {
+    props.onSelectPatient();
+  };
 
-    const select = () => {
-        props.onSelectPatient()
-    }
-
-    return (
-        <tr className='tr-lg' style={{/*  borderBottom: '0.5px solid lightgray', */ padding: 30, background: props.isSelected ? 'lightgreen' : 'white' }} onClick={() => select()}>
-            <td>
-                {props.patient.pid}
-            </td>
-            <td>
-                {props.patient.surname}
-            </td>
-            <td>
-                {props.patient.name}
-            </td>
-            <td>
-                {format(props.patient.birthdate, 'y-MM-dd')}
-            </td>
-        </tr>
-    )
+  return (
+    <tr
+      className="tr-lg"
+      style={{
+        /*  borderBottom: '0.5px solid lightgray', */ padding: 30,
+        background: props.isSelected ? "lightgreen" : "white",
+      }}
+      onClick={() => select()}
+    >
+      <td>{props.patient.pid}</td>
+      <td>{props.patient.surname}</td>
+      <td>{props.patient.name}</td>
+      <td>{format(props.patient.birthdate, "y-MM-dd")}</td>
+    </tr>
+  );
 }
--- src/physician/View/Panels/SearchPatient.jsx
+++ src/physician/View/Panels/SearchPatient.jsx
@@ -5,164 +5,166 @@ import CircularProgress from "@mui/material/CircularProgress";
 import React, { useContext, useEffect, useState } from "react";
 import { PatientContext } from "../../Model/PatientContext";
 import PatientLine from "../OtherComponents/PatientLine";
-import GeneratePatients from "../../Model/GeneratePatients";
-import Communication from '../../../common/Model/Communication'
-import Table from '@mui/joy/Table';
-import FakeSecurityModule from './../../Model/FakeSecurityModule'
+import Communication from "../../../common/Model/Communication";
+import FakeSecurityModule from "./../../Model/FakeSecurityModule";
 import MainContainer from "../../../common/View/MainContainer";
+import { RefreshButton } from "../OtherComponents/RefreshButton";
 
 export default function SearchPatient() {
+  const [patientList, setPatientList] = useState([]);
+  const [patientListToShow, setPatientListToShow] = useState([]);
+  const [loadingPatients, setLoadingPatients] = useState(false);
+  const [networkError, setNetworkError] = useState(null);
 
-    const [patientList, setPatientList] = useState(null);
-    const [patientListToShow, setPatientListToShow] = useState([]);
-    const [loadingPatients, setLoadingPatients] = useState(false)
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const { selectedPatient, setSelectedPatient } = useContext(PatientContext);
 
-    const { selectedPatient, setSelectedPatient } = useContext(PatientContext);
+  useEffect(() => {
+    getPatients();
+  }, []);
 
-    useEffect(() => {
-        setLoadingPatients(true)
-        getPatients();
+  const clearAll = () => {
+    setPatientList([]);
+    setPatientListToShow([]);
+    setNetworkError(null);
+  };
 
-    }, []);
+  const getPatients = async () => {
+    clearAll();
+    setLoadingPatients(true);
+    try {
+      const idList = await Communication.get("patient", {});
+      const namedList = await FakeSecurityModule.decriptPatients(idList);
+      namedList.sort((a, b) => a.surname.localeCompare(b.surname));
+      setPatientListToShow(namedList);
+      setPatientList(namedList);
+    } catch (err) {
+      setNetworkError(err || "Errore inatteso");
+    } finally {
+      setLoadingPatients(false);
+    }
+  };
 
-    const getPatients = async () => {
-        /* let arr = GeneratePatients(); */
-        const idArray = await Communication.get('patient', '')
-        console.log(idArray)
-        const patientArray = await FakeSecurityModule.decriptPatients(idArray)
-        patientArray.sort((a, b) => a.surname.localeCompare(b.surname));
-        setPatientList(patientArray);
-        setPatientListToShow(patientArray);
-        setLoadingPatients(false)
-    };
+  const handleSelect = () => {
+    setTimeout(() => {
+      navigate("/searchVisit");
+    }, 200);
+  };
 
-    const handleSelect = () => {
-        setTimeout(() => {
-            navigate("/searchVisit");
-        }, 200);
-    };
-
-    const research = (event) => {
-        setPatientListToShow([]);
-        let arr = patientList.filter(
-            (patient) =>
-                patient.surname
-                    .toLowerCase()
-                    .includes(event.target.value.toLowerCase()) ||
-                patient.name
-                    .toLowerCase()
-                    .includes(event.target.value.toLowerCase())
-        );
-        setPatientListToShow(arr);
-        if (event.target.value === "") setPatientListToShow(patientList);
-    };
-
-    return (
-        <div style={{ display: "flex", flexDirection: "row" }}>
-            <MainContainer>
-                <div style={{ width: '100%', display: "flex", justifyContent: "left", alignItems: "center" }}>
-                    <img src={search} width={30} height={30} />
-                    <input
-                        style={{ width: "45%", fontSize: 24 }}
-                        type="text"
-                        name="name"
-                        placeholder="cerca per nome o cognome..."
-                        onChange={(event) => research(event)}
-                    />
-
-                    <div
-                        style={{
-                            display: "flex",
-                            alignItems: "center",
-                            justifyContent: "space-between",
-                            width: "100%",
-                            paddingLeft: "2%",
-                        }}
-                    >
-                        <div>
-                            <Link
-                                to={"/newPatient"}
-                                className="btn btn-primary"
-                                style={{ fontSize: 24 }}
-                            >
-                                Nuovo paziente{" "}
-                                <img
-                                    src={add}
-                                    alt="search"
-                                    width={40}
-                                    style={{ filter: `invert(100%)` }}
-                                />
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-
-                <div
-                    style={{
-                        width: "100%",
-                        height: "72vh",
-                        overflow: "auto",
-                        textAlign: loadingPatients ? 'center' : "left",
-                        borderRadius: "15px",
-                        border: "0.5px solid #56AEC9",
-                        boxShadow: "1px 2px 6px #56AEC9",
-                    }}
-                >
-                    {loadingPatients ? <CircularProgress /> : (
-                        <table className='table table-primary table-striped table-hover'>
-                            <thead style={{ position: "sticky", top: 0, height: '6vh', }}>
-                                <tr  >
-                                    <th style={{ background: 'white' }}>id</th>
-                                    <th style={{ background: 'white' }}>Cognome</th>
-                                    <th style={{ background: 'white' }}>Nome</th>
-                                    <th style={{ background: 'white' }}>Data di nascita</th>
-
-                                </tr>
-
-                            </thead>
-
-                            <tbody >
-
-                                {patientListToShow.map((patient, index) => (
-                                    <PatientLine
-                                        key={index}
-                                        patient={patient}
-                                        isSelected={patient === selectedPatient}
-                                        onSelectPatient={() => {
-                                            setSelectedPatient(patient);
-                                            handleSelect();
-                                        }}
-                                    />
-                                ))}
-
-                            </tbody>
-                        </table>
-                    )
-                    }
-                </div>
-            </MainContainer>
-        </div>
+  const research = (event) => {
+    setPatientListToShow([]);
+    let arr = patientList.filter(
+      (patient) =>
+        patient.surname
+          .toLowerCase()
+          .includes(event.target.value.toLowerCase()) ||
+        patient.name.toLowerCase().includes(event.target.value.toLowerCase())
     );
-}
+    setPatientListToShow(arr);
+    if (event.target.value === "") setPatientListToShow(patientList);
+  };
 
-const style = {
-    box: {
-        width: "95%",
-        height: "90vh",
-        gap: "4%",
-        borderRadius: "15px",
-        background: "white",
-        margin: "auto",
-        marginTop: "1%",
-        display: "flex",
-        flexDirection: "column",
-        alignText: "left",
-        alignItems: "left",
-        padding: "2%",
-        overflow: "auto",
-        justifyContent: "start",
-        boxShadow: "1px 2px 6px #4169e1",
-    },
-};
+  return (
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <MainContainer>
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "left",
+            alignItems: "center",
+          }}
+        >
+          <img src={search} width={30} height={30} />
+          <input
+            style={{ width: "45%", fontSize: 24 }}
+            type="text"
+            name="name"
+            placeholder="cerca per nome o cognome..."
+            onChange={(event) => research(event)}
+          />
+
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              width: "100%",
+              paddingLeft: "2%",
+            }}
+          >
+            <div>
+              <Link
+                to={"/newPatient"}
+                className="btn btn-primary"
+                style={{ fontSize: 24 }}
+              >
+                Nuovo paziente{" "}
+                <img
+                  src={add}
+                  alt="search"
+                  width={40}
+                  style={{ filter: `invert(100%)` }}
+                />
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div
+          style={{
+            width: "100%",
+            height: "72vh",
+            overflow: "auto",
+            textAlign:
+              loadingPatients || networkError !== null ? "center" : "left",
+            borderRadius: "15px",
+            border: "0.5px solid #56AEC9",
+            boxShadow: "1px 2px 6px #56AEC9",
+          }}
+        >
+          {loadingPatients && <CircularProgress />}
+          {networkError !== null && (
+            <div style={{ marginTop: "1%" }}>
+              Errore nell'ottenere lista pazienti
+              <RefreshButton
+                onClick={() => {
+                  getPatients();
+                  console.log(networkError);
+                }}
+              />
+            </div>
+          )}
+          {!loadingPatients &&
+            networkError === null &&
+            patientListToShow.length > 0 && (
+              <table className="table table-primary table-striped table-hover">
+                <thead style={{ position: "sticky", top: 0, height: "6vh" }}>
+                  <tr>
+                    <th style={{ background: "white" }}>id</th>
+                    <th style={{ background: "white" }}>Cognome</th>
+                    <th style={{ background: "white" }}>Nome</th>
+                    <th style={{ background: "white" }}>Data di nascita</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {patientListToShow.map((patient, index) => (
+                    <PatientLine
+                      key={index}
+                      patient={patient}
+                      isSelected={patient === selectedPatient}
+                      onSelectPatient={() => {
+                        setSelectedPatient(patient);
+                        handleSelect();
+                      }}
+                    />
+                  ))}
+                </tbody>
+              </table>
+            )}
+        </div>
+      </MainContainer>
+    </div>
+  );
+}
