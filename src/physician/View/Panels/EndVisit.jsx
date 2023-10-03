d4ccca36f8fa6525accb66aaa7734d574e2a70f7
fridericus-hub <fed.circosta@gmail.com>
2023-10-03 18:15:18 +0200

    report page improved

--- src/physician/View/Panels/EndVisit.jsx
+++ src/physician/View/Panels/EndVisit.jsx
@@ -5,106 +5,127 @@ import { Link } from "react-router-dom";
 import "dayjs/locale/it";
 import MyDocument from "../../ViewModel/PdfCreator";
 import { useReactToPrint } from "react-to-print";
+import MainContainer from "../../../common/View/MainContainer";
+import exit from "./../../img/icon/logout.png";
+import print from "./../../img/icon/print.png";
 
 export default function EndVisit() {
-    const { newVisit, setNewVisit } = useContext(NewVisitContext);
-    const { selectedPatient, setSelectedPatient } = useContext(PatientContext);
+  const { newVisit, setNewVisit } = useContext(NewVisitContext);
+  const { selectedPatient, setSelectedPatient } = useContext(PatientContext);
 
-    const componentRef = useRef();
+  const componentRef = useRef();
 
-    const handleclick = useReactToPrint({
-        content: () => componentRef.current,
-    });
+  const handleclick = useReactToPrint({
+    content: () => componentRef.current,
+  });
 
-    return (
-        <div>
-            <div style={styles.box}>
-                <h3>Report</h3>
-                <div
-                    style={{
-                        overflow: "auto",
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        width: "80%",
-                        height: "60vh",
-                        border: "1px solid black",
-                        background: "grey",
-                    }}
-                >
-                    <MyDocument
-                        patient={selectedPatient}
-                        visit={newVisit}
-                        ref={componentRef}
-                    />
-                </div>
-
-                <div
-                    style={{
-                        display: "flex",
-                        marginBottom: "1.5%",
-                        justifyContent: "space-between",
-                        width: "40%",
-                    }}
-                >
-                    <div>
-                        <Link
-                            to={"/"}
-                            className="btn btn-danger btn-lg"
-                            onClick={() => {
-                                setNewVisit(null);
-                                setSelectedPatient(null);
-                            }}
-                        >
-                            Exit
-                        </Link>
-                    </div>
-                    <div>
-                        <button
-                            className="btn btn-success btn-lg"
-                            onClick={() => {
-                                handleclick();
-                                console.log(componentRef);
-                            }}
-                        >
-                            Save document or print
-                        </button>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <MainContainer>
+        <h3>Report</h3>
+        <div
+          style={{
+            overflow: "auto",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "80%",
+            height: "80vh",
+            borderRadius: "15px",
+            border: "0.5px solid #56AEC9",
+            boxShadow: "1px 2px 6px #56AEC9",
+            background: "grey",
+          }}
+        >
+          <MyDocument
+            patient={selectedPatient}
+            visit={newVisit}
+            ref={componentRef}
+          />
         </div>
-    );
+
+        <div
+          style={{
+            display: "flex",
+            marginBottom: "1.5%",
+            justifyContent: "space-between",
+            width: "40%",
+          }}
+        >
+          <div>
+            <Link
+              to={"/"}
+              className="btn btn-danger btn-lg"
+              onClick={() => {
+                setNewVisit(null);
+                setSelectedPatient(null);
+              }}
+            >
+              Esci{" "}
+              <img
+                src={exit}
+                alt="uscita"
+                width={38}
+                height={38}
+                style={{ filter: "invert(100%" }}
+              />
+            </Link>
+          </div>
+          <div>
+            <button
+              className="btn btn-primary btn-lg"
+              onClick={() => {
+                handleclick();
+                console.log(componentRef);
+              }}
+            >
+              <div>
+                Stampa{" "}
+                <img
+                  src={print}
+                  alt="save or print"
+                  width={38}
+                  height={38}
+                  style={{ filter: "invert(100%" }}
+                />
+              </div>
+            </button>
+          </div>
+        </div>
+      </MainContainer>
+    </div>
+  );
 }
 
 const styles = {
-    box: {
-        justifyContent: "space-around",
-        width: "98%",
-        height: "90vh",
-        borderRadius: "15px",
-        background: "white",
-        margin: "auto",
-        marginTop: "1%",
-        paddingTop: "1%",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-    },
-    container: {
-        width: "90%",
-        padding: 5,
-        height: "90%",
-        display: "flex",
-        flexDirection: "column",
-        background: "white",
-    },
-    title: {
-        fontSize: 24,
-    },
-    text: {
-        fontSize: 18,
-    },
-    link: {
-        fontSize: 12,
-    },
+  box: {
+    justifyContent: "space-around",
+    width: "98%",
+    height: "90vh",
+    borderRadius: "15px",
+    background: "white",
+    margin: "auto",
+    marginTop: "1%",
+    paddingTop: "1%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  container: {
+    width: "90%",
+    padding: 5,
+    height: "90%",
+    display: "flex",
+    flexDirection: "column",
+    background: "white",
+  },
+  title: {
+    fontSize: 24,
+  },
+  text: {
+    fontSize: 18,
+  },
+  link: {
+    fontSize: 12,
+  },
 };
--- src/physician/ViewModel/PdfCreator.jsx
+++ src/physician/ViewModel/PdfCreator.jsx
@@ -59,6 +59,83 @@ const MyDocument = forwardRef((props, ref) => {
       <div>
         <p style={styles.link}>coming soon... </p>
       </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget
+        varius nisl. Donec egestas, neque et feugiat volutpat, est mi rutrum
+        turpis, in mollis tortor justo ac justo. Maecenas ultricies pretium est.
+        Sed leo mauris, lacinia non ullamcorper ac, sollicitudin sit amet est.
+        Etiam non nisl vel purus ullamcorper feugiat. Maecenas quis est ac ex
+        mattis ullamcorper. Sed sit amet lorem leo. Cras id risus sollicitudin,
+        dictum magna eu, tempor sem. Curabitur convallis lectus ut cursus
+        consequat. Praesent in ligula commodo, facilisis turpis a, lobortis
+        purus. Phasellus suscipit feugiat aliquet. Nunc dapibus mi sem, non
+        tincidunt quam vestibulum ac. Vestibulum ut nibh enim. Donec tempus diam
+        leo, a aliquet velit vulputate non. Aliquam vel neque eget tortor
+        gravida accumsan. Morbi suscipit pulvinar massa vel euismod. Lorem ipsum
+        dolor sit amet, consectetur adipiscing elit. Suspendisse aliquam, velit
+        at interdum congue, nulla tellus sollicitudin massa, vitae porttitor
+        augue ligula sed purus. Vestibulum ullamcorper massa a nunc eleifend, ac
+        efficitur ex ullamcorper. In tortor turpis, tincidunt eget tempus vitae,
+        ullamcorper eget orci. Pellentesque viverra, est at lobortis convallis,
+        est orci laoreet sem, a vehicula lorem sapien semper massa. Etiam
+        fermentum dui interdum lacus pharetra, eget scelerisque urna mollis.
+        Curabitur molestie quam leo, sit amet ornare metus maximus nec. Mauris
+        ullamcorper sem in hendrerit placerat. Nulla orci tortor, semper vitae
+        sollicitudin sit amet, pellentesque commodo purus. Donec ultrices magna
+        ligula, sed sollicitudin ex pellentesque vel. Phasellus purus dolor,
+        mollis et imperdiet a, fringilla vel nunc. Nam viverra, massa a ultrices
+        tincidunt, mauris mi eleifend quam, vel congue erat orci sit amet ante.
+        Vestibulum ornare diam at metus molestie tristique. Morbi aliquam non
+        libero id molestie. Curabitur eleifend pretium ligula sit amet
+        elementum. Ut facilisis leo nec fermentum dignissim. Nulla sapien quam,
+        auctor at venenatis luctus, accumsan in nibh. Mauris eleifend justo
+        elementum, convallis dui sed, semper metus. Aliquam ac lectus sed erat
+        accumsan fermentum vel quis sem. In in arcu venenatis tortor elementum
+        pharetra. Ut nec placerat diam. Nullam tempus quam a purus bibendum, vel
+        imperdiet arcu dictum. Pellentesque eu sodales lectus. Ut augue tellus,
+        fringilla eget sapien quis, fringilla euismod erat. Sed ultrices nec
+        eros eu tempus. Sed ut rutrum arcu. Cras vel ligula pretium, elementum
+        leo ac, convallis mi. Sed ipsum dolor, gravida lacinia metus sit amet,
+        faucibus pulvinar quam. Quisque eget placerat tellus, molestie iaculis
+        eros. Aenean rhoncus sed dolor ut vestibulum. Sed finibus diam sit amet
+        diam egestas pharetra. Mauris urna erat, gravida sed mollis in,
+        sollicitudin eget orci. Etiam vestibulum odio a tellus euismod, sit amet
+        tincidunt risus tincidunt. Mauris tincidunt, lectus in suscipit
+        scelerisque, velit metus lacinia tortor, quis auctor enim erat eget
+        elit. Curabitur porttitor sollicitudin vestibulum. Nam justo risus,
+        congue nec diam at, dapibus pellentesque leo. Integer euismod erat
+        sapien, nec lacinia lacus aliquet et. Sed nisi lacus, porta ac massa
+        sed, gravida volutpat quam. Suspendisse potenti. Pellentesque facilisis
+        ligula nec leo dapibus fringilla. Sed porta erat vel suscipit volutpat.
+        Nullam eget erat in sem vehicula ultricies. Pellentesque habitant morbi
+        tristique senectus et netus et malesuada fames ac turpis egestas. Donec
+        tristique tellus maximus risus placerat semper. Praesent volutpat
+        sodales felis, vitae ornare ex interdum in. Cras ultricies rutrum magna,
+        ac rhoncus lacus aliquet vel. Maecenas pharetra, metus maximus vulputate
+        dictum, orci sapien bibendum turpis, sit amet vestibulum erat urna eu
+        libero. Vestibulum rutrum tincidunt quam eget tincidunt. Orci varius
+        natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+        mus. Aenean luctus sed lacus vitae pulvinar. Etiam malesuada faucibus
+        posuere. Quisque volutpat tincidunt ultrices. Maecenas commodo odio
+        posuere purus efficitur, at efficitur neque dictum. Nunc quis ante sed
+        libero pulvinar placerat. Vestibulum dictum enim ligula, nec efficitur
+        tortor porta sed. Sed quis ex ac enim aliquet blandit. Nunc vel neque
+        tellus. Aenean vulputate sem a bibendum fringilla. Vivamus volutpat
+        justo at tortor varius accumsan. Aliquam pretium elementum lacus blandit
+        consequat. Morbi id pulvinar velit. Cras vehicula, sapien a euismod
+        laoreet, ante odio elementum purus, vel ultrices felis magna a turpis.
+        Cras viverra dolor sit amet arcu viverra, ut molestie lorem tempus.
+        Fusce accumsan rutrum risus, at pulvinar dolor elementum quis. Aenean
+        sed mattis enim, ut sodales metus. Vestibulum eleifend elit fringilla
+        dui mattis, eget finibus mauris vestibulum. Sed eu lectus libero.
+        Vestibulum non consequat metus. Morbi luctus pharetra ex at gravida.
+        Morbi gravida iaculis mauris vel feugiat. Duis lacinia ac eros non
+        blandit. Nulla tempus tortor et mauris feugiat, vitae consequat leo
+        ultrices. Proin vitae felis nisi. Donec ut libero accumsan, pulvinar
+        nisi eu, rutrum nisi. Vestibulum lacus quam, euismod at vestibulum eget,
+        eleifend nec tortor. Vivamus pharetra vehicula massa, bibendum finibus
+        nibh sollicitudin quis.
+      </div>
     </div>
   );
 });
