879185a4f913335231333fa20e82e9bcb59d943e
Matteo Manzoni <matteo.manzoni2@studenti.unimi.it>
2023-11-01 17:41:16 +0100

    change webapp endpoint

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 const config = {
-    ENDPOINT: "apolloWebAppTest",
+    ENDPOINT: "apollo",
     MAIN_API_URL: "https://apollo.policlinico.mi.it/apiV3",
     DEANONYMIZED_API_URL: "https://apollo.policlinico.mi.it/apiV3/deanonymized",
     LOCAL_STORAGE_SESSION_KEY: "ApolloSession",
